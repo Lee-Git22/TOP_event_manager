1e66b7a4703151ec2eea9172f10c044e03bc426c
Lee-Git22 <lee.alan9801@gmail.com>
2023-04-05 15:26:28 -0400

    Implement phone num assignment

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -3,7 +3,25 @@ require 'google/apis/civicinfo_v2'
 require 'erb'
 
 def clean_zipcode(zipcode)
-  zipcode.to_s.rjust(5,"0")[0..4]
+  zipcode.to_s.rjust(5, "0")[0..4]
+end
+
+def clean_home_phone(home_phone)
+  # Scans for digits and joins it as new string
+  home_phone = home_phone.scan(/\d/).join
+
+  # Checks valid number condition
+  if home_phone.to_s.length == 11 && home_phone.to_s[0] == '1'
+    home_phone[1..]
+  elsif home_phone.to_s.length == 10
+    home_phone
+  else
+    'Invalid HomePhone'
+  end
+
+end
+
+def fetch_hour_target(regdate)
 end
 
 def legislators_by_zipcode(zip)
@@ -21,7 +39,7 @@ def legislators_by_zipcode(zip)
   end
 end
 
-def save_thank_you_letter(id,form_letter)
+def save_thank_you_letter(id, form_letter)
   Dir.mkdir('output') unless Dir.exist?('output')
 
   filename = "output/thanks_#{id}.html"
@@ -46,6 +64,10 @@ contents.each do |row|
   id = row[0]
   name = row[:first_name]
   zipcode = clean_zipcode(row[:zipcode])
+  home_phone = clean_home_phone(row[:homephone])
+
+  hour_target = fetch_hour_target(row[:regdate])
+
   legislators = legislators_by_zipcode(zipcode)
 
   form_letter = erb_template.result(binding)
